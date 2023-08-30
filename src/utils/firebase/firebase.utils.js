bb5ba8ecf9a5ddd11703e8570643e58dac526a84
lorandblzs <lorand.blzs@gmail.com>
2023-08-30 20:03:41 +0300

    aded sign-in componenet, finished styling sing-in and sign-up styles

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,7 +3,9 @@ import { getAuth,
          signInWithRedirect, 
          signInWithPopup, 
          GoogleAuthProvider, 
-         createUserWithEmailAndPassword } from 'firebase/auth';
+         createUserWithEmailAndPassword,
+         signInWithEmailAndPassword
+        } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -61,4 +63,10 @@ const firebaseConfig = {
     if(!email || !password) return;
 
     return await createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  export const  signInAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password);
   };
