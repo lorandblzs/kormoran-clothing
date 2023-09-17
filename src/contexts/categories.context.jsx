c106fc97a14b91036d3f80fdfa8cfea92e63ae85
lorandblzs <lorand.blzs@gmail.com>
2023-09-17 20:53:14 +0300

    moved products database to Firbase and refactored the code to fetch the data from there

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,29 @@
+import { createContext, useState, useEffect } from "react";
+
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
+
+
+export const CategoriesContext = createContext({
+    categoriesMap: {},
+
+});
+
+export const CategoriesProvider = ({children}) => {
+    const[categoriesMap, setCategoriesMap] = useState({});
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categotyMap = await getCategoriesAndDocuments();
+            console.log(categotyMap);
+            setCategoriesMap(categotyMap);
+        };
+        getCategoriesMap();
+    },  []);
+
+
+    const value = {categoriesMap};
+
+    return (
+        <CategoriesContext.Provider value={value}> {children} </CategoriesContext.Provider>
+    )
+}
