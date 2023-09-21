a4dd386376808d5babf57f0aceffac501181d382
lorandblzs <lorand.blzs@gmail.com>
2023-09-21 20:50:29 +0300

    -

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -4,7 +4,7 @@ import { CartContext } from '../../contexts/cart.context';
 
 import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component';
 
-import './product-card.styles.scss';
+import './product-card.styles.scss'
 
 
 const ProductCard = ({ product }) => {
@@ -13,7 +13,8 @@ const ProductCard = ({ product }) => {
 
    const addProductToCart = () => addItemToCart(product);
 
-   return (<div className='product-card-container'>
+   return (
+   <div className='product-card-container'>
         <img src={imageUrl} alt={`${name}`} />
         <div className='footer'>
             <span className='name'>{name}</span>
