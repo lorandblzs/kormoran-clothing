9760199ef139c914de35f28ef8b657d330735e3d
lorandblzs <lorand.blzs@gmail.com>
2023-10-01 15:38:50 +0300

    small update

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -2,23 +2,23 @@ import { Link } from 'react-router-dom';
 
 import ProductCard from '../product-card/product-card.component';
 
-import './category-preview.styles.scss';
+import { Preview, Title, CategoryPreviewContainer  } from './category-preview.styles';
 
 const CategoryPreview = ( {title, products} ) => {
     return (
-        <div className='category-preview-container'>
-            <h2>
-                <Link className='title' to={title}>{title.toUpperCase()}</Link>
-            </h2>
-            <div className='preview'>
+        <CategoryPreviewContainer>
+            <Title>
+                <Link to={title}>{title.toUpperCase()}</Link>
+            </Title>
+            <Preview>
                 {
                     products
                         .filter((_, idx) => idx < 4)
                         .map((product) => 
                         <ProductCard key={product.id} product={product}/> )
                 }
-            </div>
-        </div>
+            </Preview>
+        </CategoryPreviewContainer>
     )
 }
 
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -4,7 +4,7 @@ import { CartContext } from '../../contexts/cart.context';
 
 import Button, {BUTTON_TYPE_CLASSES} from '../button/button.component';
 
-import './product-card.styles.scss'
+import { ProductCardContainer, Img, Footer, Name, Price } from './product-card.styles'
 
 
 const ProductCard = ({ product }) => {
@@ -14,14 +14,15 @@ const ProductCard = ({ product }) => {
    const addProductToCart = () => addItemToCart(product);
 
    return (
-   <div className='product-card-container'>
-        <img src={imageUrl} alt={`${name}`} />
-        <div className='footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
-        </div>
+    
+   <ProductCardContainer>
+        <Img src={imageUrl} alt={`${name}`} />
+        <Footer>
+            <Name>{name}</Name>
+            <Price>{price}</Price>
+        </Footer>
         <Button buttonType={BUTTON_TYPE_CLASSES.inverted} onClick={addProductToCart} >Add to cart</Button>
-    </div>)
+    </ProductCardContainer>)
 };
 
 export default ProductCard;
