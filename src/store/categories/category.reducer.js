f75451918bbb0c507a81d05f6396520cced94008
lorandblzs <lorand.blzs@gmail.com>
2023-11-07 21:28:07 +0200

    progress with Redux

--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,8 +1,8 @@
 import { CATEGORIES_ACTION_TYPE } from "./category.types";
 
 export const CATEGORIES_INITIAL_STATE = {
-    categoriesMap: {}
-}
+    categories: [],
+};
 
 export const categoriesReducer = (
     state = CATEGORIES_INITIAL_STATE, 
@@ -11,8 +11,8 @@ export const categoriesReducer = (
     const { type, payload } = action;
 
     switch(type) {
-        case CATEGORIES_ACTION_TYPE.SET_CATEGORIES_MAP:
-            return{...state, categoriesMap: payload};
+        case CATEGORIES_ACTION_TYPE.SET_CATEGORIES:
+            return{...state, categories: payload};
         default:
             return state;    
     }
