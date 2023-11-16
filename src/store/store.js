dee1d7fd02041d2861f22a4907f174645a070220
lorandblzs <lorand.blzs@gmail.com>
2023-11-16 18:40:07 +0200

    implemented thunks, added spinner when loading categories prewiev and products

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { compose, legacy_createStore as createStore, applyMiddleware } from "red
 import { persistStore, persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage';
 import logger from "redux-logger";
+import thunk from 'redux-thunk';
 
 import { rootReducer } from "./root-reducer";
 
@@ -9,14 +10,14 @@ import { rootReducer } from "./root-reducer";
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['user']
-}
+    whitelist: ['cart'],
+};
 
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
 
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
